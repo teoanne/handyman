6535f5a5948609140fbfd4214c508754ac85dc6e
Sue Anne <sueanneteo@gmail.com>
2014-01-08 22:06:11 +0100

    added devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Handyman::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Note to set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'handyman-sg.herokuapp.com' }
 end
